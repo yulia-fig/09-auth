4280a2ecf3de15eccf6f3c7f5389267cfc548149
Yulia Fig <fihlevska@gmail.com>
2026-06-18 23:13:17 +0200

    9-hw-next

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -2,7 +2,7 @@
 import css from './NoteForm.module.css';
 import { useId, useState, useEffect } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { createNote } from '../../lib/api';
+import { createNote } from '../../lib/api/clientApi';
 import { type NoteTag } from '../../types/note';
 import { useRouter } from 'next/navigation';
 import { useNoteDraftStore } from '@/lib/store/noteStore';
--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -1,7 +1,7 @@
 import css from "./NoteList.module.css";
 import type { Note } from "../../types/note";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { deleteNote } from "../../lib/api";
+import { deleteNote } from "../../lib/api/clientApi";
 import Link from "next/link";
 interface NoteListProps {
   notes: Note[];
