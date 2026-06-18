9fe89655540ebb357c4ae9ed8f71a1412d5daed7
Yulia Fig <fihlevska@gmail.com>
2026-06-18 23:15:12 +0200

    9-hw-next

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { CreateNoteDto } from '../api';
+import { CreateNoteDto } from '../api/clientApi';
 import { persist } from 'zustand/middleware';
 
 type NoteDraftStore = {
